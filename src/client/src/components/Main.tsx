9fd651e02036cc9be2df293f8dee5dd5fff8b5b1
tawny <hitawnie@gmail.com>
2023-08-13 00:13:44 -0700

    hackin

--- src/client/src/components/Main.tsx
+++ src/client/src/components/Main.tsx
@@ -1,13 +1,43 @@
+import Clerk from "@clerk/clerk-js";
 import type { Component } from 'solid-js';
+import { createEffect } from 'solid-js';
 
 import styles from '../styles/Main.module.css';
 
+const publishableKey = "pk_test_cHJlY2lvdXMtcmVwdGlsZS0zMy5jbGVyay5hY2NvdW50cy5kZXYk";
+const clerk = new Clerk(publishableKey);
+await clerk.load({
+  // Set load options here...
+});
+
 const Main: Component = () => {
+  let signInDiv: HTMLDivElement;
+  let signUpDiv: HTMLDivElement;
+  let userButtonDiv: HTMLDivElement;
+
+  // Mount the appropriate components based on the user's sign-in status
+  createEffect(() => {
+    if (clerk.user) {
+      // If the user is signed in, mount the UserButton
+      if (userButtonDiv) clerk.mountUserButton(userButtonDiv);
+    } else {
+      // If the user is not signed in, mount the sign-in and sign-up components
+      if (signInDiv) clerk.mountSignIn(signInDiv);
+      if (signUpDiv) clerk.mountSignUp(signUpDiv);
+    }
+  });
+
   return (
     <div class={styles.Main}>
-      <header class={styles.header}>
-        Hey
-      </header>
+      <header class={styles.header}>Hey</header>
+      {clerk.user ? (
+        <div ref={(el) => (userButtonDiv = el as HTMLDivElement)} /> /* This div will contain the UserButton component */
+      ) : (
+        <>
+          <div ref={(el) => (signInDiv = el as HTMLDivElement)} /> {/* This div will contain the sign-in component */}
+          <div ref={(el) => (signUpDiv = el as HTMLDivElement)} /> {/* This div will contain the sign-up component */}
+        </>
+      )}
     </div>
   );
 };
--- src/server/api/hello.ts
+++ src/server/api/hello.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from 'express';
 const router = Router();
 
 router.get('/hello', (req: Request, res: Response) => {
-  res.send('Hello, world!');
+  res.send('Hello, world! hey');
 });
 
 export default router;
