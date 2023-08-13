4ec7b97187d4fbc575de0517bbcd6c9902ee3cf8
tawny <hitawnie@gmail.com>
2023-08-13 00:31:29 -0700

    remove custom auth

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,7 +1,5 @@
 import express from 'express';
 import helloRouter from './api/hello';
-import register from './api/register';
-import login from './api/login';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,10 +8,8 @@ app.use(express.json());
 
 // Register the route handlers
 app.use('/api', helloRouter);
-app.use('/api', register);
-app.use('/api', login);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
-  console.log('route stack', app._router.stack)
+  // console.log('route stack', app._router.stack)
 });
