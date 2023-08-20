50ed097149df9fbf73439d564de72bbd487f4095
tawny <hitawnie@gmail.com>
2023-08-20 02:29:40 -0700

    homies final fix_v2_test_works

--- src/client/src/components/Main.tsx
+++ src/client/src/components/Main.tsx
@@ -1,15 +1,27 @@
-import { Component } from 'solid-js';
-import  Stripe from './Stripe';
+import { Component, createSignal } from 'solid-js';
+import Stripe from './Stripe';
 import ClerkComponent from './Clerk';
 import booker from '../assets/booker.png';
+import homies from '../assets/homies.png';
 
 import styles from '../styles/Main.module.css';
 
 const Main: Component = () => {
+  // Create a piece of reactive state to manage the image visibility
+  const [showHomies, setShowHomies] = createSignal(false);
+
+  const allMyHomies = () => {
+    setShowHomies(!showHomies()); // Toggle the state
+  }
+
   return (
     <div class={styles.Main}>
       <img src={booker} alt="Booker" />
-      <header class={styles.header}>Hey</header>
+      <button onclick={allMyHomies} class={styles.header}>Hey</button>
+      
+      {/* Conditionally render the image based on the state */}
+      {showHomies() && <img src={homies} alt="homies"/>}
+
       <ClerkComponent />
       <Stripe />
     </div>
